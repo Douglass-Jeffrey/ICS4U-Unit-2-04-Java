a59d1a917f6d2c2b6e8041fc52c0feb181f92184
Ubuntu <ubuntu@ip-172-31-35-94.ec2.internal>
2020-12-11 00:06:09 +0000

    IC

---
+++ MrCoxallStackV4.java
@@ -0,0 +1,70 @@
+/* 
+* This class creates an arraylist that will recieve input from  class
+*
+* @author  Douglass Jeffrey
+* @version 1.0
+* @since   2020-12-07
+*/
+
+// Import the ArrayList class
+import java.util.ArrayList;
+
+public class MrCoxallStackV4 {
+    
+  // Creates arraylist that will act as a stack
+  private ArrayList<Integer> stackAsArray = new ArrayList<Integer>();
+
+  // Method for adding inputs to list
+  public void push(int receivedNum) {
+    stackAsArray.add(receivedNum);
+  }
+
+  /**
+   * pop method to return top number on stack and remove it.
+   */
+
+  public int pop() {
+    if (this.stackAsArray.size() == 0) {
+      throw null;
+    } else {
+      int poppedNumber = this.stackAsArray.get(stackAsArray.size() - 1);
+      stackAsArray.remove(stackAsArray.size() - 1);
+      return poppedNumber;
+    }
+  }
+
+  /**
+   * peek method to observe top number on stack without removing it.
+   */
+  public int peek() {
+    if (this.stackAsArray.size() == 0) {
+      throw null;
+    } else {
+      int peekNumber = this.stackAsArray.get(stackAsArray.size() - 1);
+      return peekNumber;
+    }
+  }
+
+  /**
+   * Clears the stack of all values when called.
+   */
+  public String clearer() {
+    stackAsArray.clear();
+    String arrayClearCommand = "Array cleared Successfully! ";
+    return arrayClearCommand;
+  }
+
+
+  /**
+   * Getter that tells the user all values in the stack.
+   */
+
+  public String currentStack() {
+    String arrayVal = "Current ArrayList: ";
+    for (int counter = 0; counter < stackAsArray.size(); counter++) {
+      arrayVal = arrayVal + stackAsArray.get(counter) + ", ";
+    }
+    return arrayVal;
+  }
+}
+
---
+++ StackInputV4.java
@@ -0,0 +1,115 @@
+/*
+* This program pushes a user input to a program with a class containing an 
+* arraylist. The most recent input of the arraylist is then pushed back from
+* that class into this program and is outputted, mimicking how the stack works.
+* an input is now popped from the stack and returned to the user.
+* Now it is impervious to any type of error. Has peek and clear methods now.
+*
+* @author  Douglass Jeffrey
+* @version 1.0
+* @since   2020-12-07
+*/
+
+// Import the Scanner class
+import java.util.Scanner;
+
+public class StackInputV4 {
+
+  /**
+  * This program recieves input and adds values onto our pseudo-stack.
+  */
+
+  public static void main(String[] args) {
+
+    // create scanner object
+    Scanner userInput = new Scanner(System.in);
+    
+    /* creating a second scanner object because for some reason the first
+    * buggs out after being used in the clearinput section of the code.
+    */
+    
+    Scanner scanClear = new Scanner(System.in);
+
+    // references MrCoxallStack file 
+    MrCoxallStackV4 clasStack = new MrCoxallStackV4();
+
+    // ensures that user inputs only integer values
+    try {
+      // accepts input on how many integers program should accept
+      System.out.println("Enter the amount of integers you wish to push: ");
+      int stackNum = userInput.nextInt();
+      
+      // checks if inputs are valid
+      if (stackNum < 1) {
+        System.out.println("Invalid Input. Input only integers above 0");
+      } else {
+        for (int counter = 0; counter < stackNum; counter++) {
+
+          // receive user input
+          System.out.println("Enter an integer to push onto the stack ");
+          int numberChosen = userInput.nextInt();
+          System.out.println();
+
+          // Pushes user input into arraylist in MrCoxallStack 
+          clasStack.push(numberChosen);
+        }
+
+        // finds amount of numbers user wishes to pop
+        System.out.println("Enter the amount of integers you wish to pop: ");
+        int popNum = userInput.nextInt();
+
+        // checks if inputs are valid
+        if (popNum < 1) {
+          System.out.println("Invalid pop number assuming you want"
+                             + " nothing popped...");
+        } else {
+
+          // executes popping popNum amount of times
+          for (int popCount = 0; popCount < popNum; popCount++) {
+
+            // pops top number in stack
+            System.out.println("Popped Num: " + clasStack.pop());
+          }
+        }
+        
+        // peeking num
+        System.out.println();
+        System.out.println("Peeked Num: " + clasStack.peek());
+
+        // allows user to clear if they choose
+        System.out.println();
+        System.out.println("Would you like to clear the stack? (Yes/No) ");
+        String clearInputLow = scanClear.nextLine();
+        
+        // converts input to upper to prevent case based errors
+        String clearInput = clearInputLow.toUpperCase();
+        
+        // checks if user wants to clear
+        if (clearInput.equals("YES")) {
+          System.out.println();
+          // if they want to clear call clear method
+          System.out.println(clasStack.clearer());
+        // if they dont want to clear acknowledge their response
+        } else if (clearInput.equals("NO")) {
+          System.out.println();
+          System.out.println("Answer acknowledged, continuing ...");
+        // if they input something wrong, tell them
+        } else {
+          System.out.println();
+          System.out.println("Invalid input, presuming you dont want to "
+                             + "clear");
+        }
+
+        // states current values located in stack
+        System.out.println();
+        System.out.println(clasStack.currentStack());
+      }
+    // catch invalid errors. this one for if null is returned.
+    } catch (NullPointerException e) {
+      System.out.println("Invalid Input. More numbers Popped than in stack");
+    // this one for general problems like bad user inputs
+    } catch (Exception e) {
+      System.out.println("Invalid Input");
+    }
+  }
+}
